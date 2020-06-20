b82f924aaff3b56e57b5bcb646d13bfccab82e45
Evan Burton <evan@heyevan.com>
2020-06-20 20:55:38 +0000

    first commit

---
+++ lib/KaraokeHellYeah.rb
@@ -0,0 +1,6 @@
+require "KaraokeHellYeah/version"
+
+module KaraokeHellYeah
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/KaraokeHellYeah/version.rb
@@ -0,0 +1,3 @@
+module KaraokeHellYeah
+  VERSION = "0.1.0"
+end
