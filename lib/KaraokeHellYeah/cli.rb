76805346985a30f2ebe3665ed54fa72e5994a5fb
Evan Burton <evan@heyevan.com>
2020-07-25 23:52:00 +0000

    top song method working

--- lib/KaraokeHellYeah/cli.rb
+++ lib/KaraokeHellYeah/cli.rb
@@ -1,30 +1,24 @@
 class KaraokeHellYeah::CLI
 
-  def call
-    puts "Hello, welcome to KaraokeHellYeah. These are the top songs of the week.
-    Enter 1-10 to see the lyrics to your new favorite song ;)."
-    #get_lyrics_for(song)
-    #list_lyrics
+    def call
+      puts "Hello, welcome to KaraokeHellYeah. These are the top songs of the week.
+      Enter 1-10 to see the lyrics to your new favorite song ;)."
     get_top_songs
-  end
+    list_songs
+    end
 
-  def get_top_songs
-    #to be scraped
-    @songs = ['Ballin', "FunkFlex", "Jazz","Coldstone"]
-  end
-binding.pry
+    def get_top_songs
+      #to be scraped
+      @songs = ['Ballin', "FunkFlex", "Jazz","Coldstone"]
+    end
 
-  def get_user_song
-  #list_songs
-    @songs.each.with_index() do  |song, index|
-    puts "#{index +1}", "#{song}"
- end
-
-def list_months
-  @months.each_with_index do |month, index|
-    puts "#{index}", "#{month}"
+    def list_songs
+      @songs.each.with_index(1) do |month, index|
+        puts "#{index}. #{month}"
+    end
   end
 end
+
 
 #   def list_songs
 #     songs = ['']
