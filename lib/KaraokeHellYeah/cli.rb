7eb438f2d8ff6343a2efe539433cd62881541583
Evan Burton <evan@heyevan.com>
2020-07-25 22:21:19 +0000

    writing CLI

--- lib/KaraokeHellYeah/cli.rb
+++ lib/KaraokeHellYeah/cli.rb
@@ -1,15 +1,25 @@
 class KaraokeHellYeah::CLI
 
-def call 
-  
-puts "Hello, welcome to KaraokeHellYeah. These are the top songs of the week.
-Enter 1-10 to see that song's lyrics."  
-  end 
-end 
+  def call
+    puts "Hello, welcome to KaraokeHellYeah. These are the top songs of the week.
+    Enter 1-10 to see the lyrics to your new favorite song ;)."
+    #get_lyrics_for(song)
+    #list_lyrics
+  end
 
+  def get_top_songs(rank, title, artist)
+    #to be scraped
+    @songs = {rank => "1", title => "butter", artist => "me"}
+    @songs
+  end
+  binding.pry
 
+  def get_user_song
+    songs = ['']
+  end
 
-
-
-  
-  
+  def list_songs
+    songs = ['']
+    songs
+  end
+end
