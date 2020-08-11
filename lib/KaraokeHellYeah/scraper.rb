db29e9247d112146906b2ff40b4e921167752a94
Evan Burton <evan@heyevan.com>
2020-08-11 04:25:22 +0000

    adds more scraping functionality

--- lib/KaraokeHellYeah/scraper.rb
+++ lib/KaraokeHellYeah/scraper.rb
@@ -4,16 +4,18 @@ class KaraokeHellYeah::Scraper
     page = Nokogiri::HTML(open("https://genius.com/#top-songs")) 
   
     songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
-    maker = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
+    artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
   
     songs.each do |song|
     name = song.text
-    
-    maker.each do |singer|
-    artist = singer.text 
-      
-      KaraokeHellYeah::Songs.new(name, artist)
-      end
+   binding.pry
+   end
+   
+    artists.each do |artist|
+    artist = artist.text
+  
+ KaraokeHellYeah::Songs.new(name, artist)
+     
     end 
   end 
 end 
