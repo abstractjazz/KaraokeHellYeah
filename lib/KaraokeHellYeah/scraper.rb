eaf90c1b134ed3a31098b1152adb5c0da334fd13
Evan Burton <evan@heyevan.com>
2020-08-11 05:02:29 +0000

    corrects scraper functionality

--- lib/KaraokeHellYeah/scraper.rb
+++ lib/KaraokeHellYeah/scraper.rb
@@ -3,28 +3,16 @@ class KaraokeHellYeah::Scraper
   def self.scrape_songs
     page = Nokogiri::HTML(open("https://genius.com/#top-songs")) 
   
-    songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
-    artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
+    @songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
+    @artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
   
-    songs.each do |song|
+    @songs.zip @artists
+    @songs.zip(@artists).each do |song, artist|
     name = song.text
-   binding.pry
-   end
-   
-    artists.each do |artist|
     artist = artist.text
   
- KaraokeHellYeah::Songs.new(name, artist)
-     
+  KaraokeHellYeah::Songs.new(name, artist)
     end 
   end 
 end 
 
- 
-  
-  
-  
-# def scrape_lyrics 
-  
-# end 
-# end 
