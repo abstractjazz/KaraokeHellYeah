dd828244a8015be162d73d04aa9fa6e484fff68e
Evan Burton <evan@heyevan.com>
2020-09-08 02:17:58 +0000

    adds exit flow control to cli

--- lib/KaraokeHellYeah/cli.rb
+++ lib/KaraokeHellYeah/cli.rb
@@ -6,41 +6,54 @@ class KaraokeHellYeah::CLI
     list_songs
     get_user_song
     end
-
+    
     def get_top_songs
-      #to be scraped
-      # KaraokeHellYeah::Songs.new("Cool", "Asap")
-      # KaraokeHellYeah::Songs.new("Okay", "Frank Ocean")
-      @songs = KaraokeHellYeah::Songs.all 
+   @songs = KaraokeHellYeah::Songs.all 
     end
 
     def list_songs
-      puts "Type a number 1-10 & get the lyrics to your new favorite song.\n" 
+      puts "\nType a number 1-10 & get the lyrics to your new favorite song.\n" 
       @songs.each.with_index(1) do |song, index|
-        #binding.pry
-        puts "\n#{index}. #{song.name} - #{song.artist} #{song.url}"
+        puts "\n#{index}. #{song.name} - #{song.artist} #{song.url}\n"
     end
   end
 
-
-    def get_user_song 
-    @url = KaraokeHellYeah::Scraper.scrape_lyrics
-    chosen_song = gets.strip.to_i
-   get_lyrics_for(chosen_song) if valid_input(chosen_song, @songs)
+  def get_user_song 
+  chosen_song = gets.strip.to_i
+  if valid_input(chosen_song, @songs)
+  get_lyrics_for(chosen_song) 
+  else 
+   puts "Only the hits. Choose a number from 1-10."
+   sleep 3
+   call
    end
+ end 
 
     def valid_input (user_input, data)
     user_input.to_i <= data.length && user_input.to_i > 0
     end
-
-  def get_lyrics_for(chosen_song)
-  song = @songs[chosen_song -1]
-  puts "Here are the lyrics to #{song.name}"
-  @lyrics = KaraokeHellYeah::Scraper.scrape_lyrics  
-  @url = @lyrics[chosen_song-1]
-  page = Nokogiri::HTML(open(@url))
-  puts page.css(".lyrics").text
-  end
+    
+ def get_lyrics_for(chosen_song)
+ song = @songs[chosen_song -1]
+ puts "Press enter to see the lyrics for #{song.name}"
+ @lyrics = KaraokeHellYeah::Scraper.scrape_lyrics
+  puts @lyrics
+  puts "Would you like the lyrics to another song? (y/n)"
+  user_response = gets.strip 
+  if user_response == "n"
+  puts "Later!"
+  elsif user_response == "y" 
+  call
+  elsif user_response == /\b(?!y|n)\b\S+/
+  puts "Sorry too busy jammin' to understand. Please type only the letter (y/n)." 
+  sleep 4
+  list_songs 
+  else 
+end
 end 
- 
-
+  
+   def self.chosen_song
+  chosen_song = gets.strip.to_i
+  chosen_song
+ end 
+end 
--- lib/KaraokeHellYeah/scraper.rb
+++ lib/KaraokeHellYeah/scraper.rb
@@ -3,34 +3,29 @@ class KaraokeHellYeah::Scraper
   
   def self.scrape_songs
     page = Nokogiri::HTML(open("https://genius.com/#top-songs")) 
-    # chosen_song = gets.strip.to_i
+   
   
     @songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
     @artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
   
     
-    # @url = page.css("div#top-songs a")[lyric_selector]["href"]
-    # # binding.pry
   
     @songs.zip @artists
     @songs.zip(@artists).each do |song, artist|
     name = song.text
     artist = artist.text
-    
+
     
   def self.scrape_lyrics
   page = Nokogiri::HTML(open("https://genius.com/#top-songs")) 
   @url = page.css('div#top-songs a').map {|link| link['href']}
-  #page.css(".lyrics").text
+  user_selection = KaraokeHellYeah::CLI.chosen_song
+  @print_lyrics = @url[user_selection -1]
+  scrape_2 = Nokogiri::HTML(open(@print_lyrics))
+  puts scrape_2.css(".lyrics").text
   end 
     
-    
-    def self.lyrics 
-    
-    
-    # binding.pry
-    end 
-  
+   
   KaraokeHellYeah::Songs.new(name,artist,@url)
     end
   end 
