231ed2b5893b85719664f6bd77fd440272d72aaa
Evan Burton <evan@heyevan.com>
2020-09-13 22:13:14 +0000

    deletes unnessary line in scraper method

--- lib/KaraokeHellYeah/scraper.rb
+++ lib/KaraokeHellYeah/scraper.rb
@@ -4,7 +4,7 @@ class KaraokeHellYeah::Scraper
  page = Nokogiri::HTML(open("https://genius.com/#top-songs")) 
  @songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
  @artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
- @songs.zip @artists
+ 
  @songs.zip(@artists).each do |song, artist|
  name = song.text
  artist = artist.text
