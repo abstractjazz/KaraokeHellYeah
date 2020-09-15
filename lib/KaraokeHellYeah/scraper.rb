22116f3fdaea6fce74b1f29bee490fa8e940c0ff
Evan Burton <evan@heyevan.com>
2020-09-15 04:01:16 +0000

    removes unnessary instance variables

--- lib/KaraokeHellYeah/scraper.rb
+++ lib/KaraokeHellYeah/scraper.rb
@@ -2,10 +2,10 @@ class KaraokeHellYeah::Scraper
 
  def self.scrape_songs
  page = Nokogiri::HTML(open("https://genius.com/#top-songs")) 
- @songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
- @artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
+ songs = page.css(".ChartSongdesktop__Title-sc-18658hh-3")
+ artists = page.css(".ChartSongdesktop__Artist-sc-18658hh-5")
  
- @songs.zip(@artists).each do |song, artist|
+ songs.zip(artists).each do |song, artist|
  name = song.text
  artist = artist.text
  KaraokeHellYeah::Songs.new(name,artist)
